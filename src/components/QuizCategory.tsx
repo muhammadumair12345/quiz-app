0380e7ce082e07814face00c31184f1afdbdfb0a
Muhammad Umair <hafizumairtariq1234@gmail.com>
2021-05-25 02:02:13 +0500

    updated

--- src/components/QuizCategory.tsx
+++ src/components/QuizCategory.tsx
@@ -7,12 +7,9 @@ const QuizCategory = () => {
             <h3>Select Quiz Category</h3>
             <select name="pets">
                 <option value="">Choose Category</option>
-                <option value="dog">Dog</option>
-                <option value="cat">Cat</option>
-                <option value="hamster">Hamster</option>
-                <option value="parrot">Parrot</option>
-                <option value="spider">Spider</option>
-                <option value="goldfish">Goldfish</option>
+                <option value="computer">Computer</option>
+                <option value="maths">Maths</option>
+                <option value="science">Science</option>
             </select>
             <h3>Select Difficulty Level</h3>
             <button className="easy-btn">Easy</button>
