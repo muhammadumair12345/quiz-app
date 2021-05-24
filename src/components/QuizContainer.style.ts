a37a765aed9f489de0e09d8cf519dcb08e22ee2e
Muhammad Umair <hafizumairtariq1234@gmail.com>
2021-05-25 01:41:59 +0500

    updated

--- src/components/QuizContainer.style.ts
+++ src/components/QuizContainer.style.ts
@@ -4,10 +4,13 @@ export const Container = styled.div`
     background-color:rgb(254 128 0 / 0.6);
     box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
     min-height: 500px;
-    min-width: 350px;
+    width: 400px;
     padding:1rem;
     border:4px solid #fff;
     border-radius: 15px;
     display: flex;
     flex-direction: column;
+    @media (max-width: 400px) {
+        width:100vw;
+     }
 `;
